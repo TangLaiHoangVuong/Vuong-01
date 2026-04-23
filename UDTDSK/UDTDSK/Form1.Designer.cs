b398e69d99965a9234ebb7438a91f07ad68898ec
vuong022101009@tgu.edu.vn <vuong022101009@tgu.edu.vn>
2026-04-23 10:40:27 +0700

    123

--- UDTDSK/UDTDSK/Form1.Designer.cs
+++ UDTDSK/UDTDSK/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(105, 112);
+            this.label1.Location = new System.Drawing.Point(30, 104);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(391, 25);
             this.label1.TabIndex = 0;

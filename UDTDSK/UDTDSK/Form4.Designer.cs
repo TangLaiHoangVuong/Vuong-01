4738054b2262dbe67d617e7bbda1577bb8909d7a
vuong022101009@tgu.edu.vn <vuong022101009@tgu.edu.vn>
2026-05-07 09:34:52 +0700

    Màng hình chính

--- UDTDSK/UDTDSK/Form4.Designer.cs
+++ UDTDSK/UDTDSK/Form4.Designer.cs
@@ -35,8 +35,11 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -50,80 +53,122 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.splitContainer1.Panel1.Controls.Add(this.btnDangXuat);
             this.splitContainer1.Panel1.Controls.Add(this.button5);
             this.splitContainer1.Panel1.Controls.Add(this.button4);
             this.splitContainer1.Panel1.Controls.Add(this.button3);
             this.splitContainer1.Panel1.Controls.Add(this.button2);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
             this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(547, 351);
-            this.splitContainer1.SplitterDistance = 182;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(754, 380);
+            this.splitContainer1.SplitterDistance = 236;
             this.splitContainer1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::UDTDSK.Properties.Resources.ba_con_thằn_lằn;
-            this.pictureBox1.Location = new System.Drawing.Point(22, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(35, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(135, 74);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_MouseEnter);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 99);
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(22, 112);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 33);
+            this.button1.Size = new System.Drawing.Size(167, 33);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Thông tin cá nhân";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(24, 150);
+            this.button2.BackColor = System.Drawing.Color.SteelBlue;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(24, 163);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 33);
+            this.button2.Size = new System.Drawing.Size(165, 33);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Quản lý dữ liệu sức khoẻ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(22, 202);
+            this.button3.BackColor = System.Drawing.Color.SteelBlue;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(22, 215);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 33);
+            this.button3.Size = new System.Drawing.Size(167, 33);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Text = "Quản lý phân tích tính toán";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(24, 256);
+            this.button4.BackColor = System.Drawing.Color.SteelBlue;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(24, 269);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(135, 33);
+            this.button4.Size = new System.Drawing.Size(165, 33);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Text = "Quản lý mục tiêu và theo dõi";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(22, 300);
+            this.button5.BackColor = System.Drawing.Color.SteelBlue;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(22, 319);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(135, 33);
+            this.button5.Size = new System.Drawing.Size(167, 33);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Text = "Quản lý cảnh báo";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnDangXuat.ForeColor = System.Drawing.Color.White;
+            this.btnDangXuat.Location = new System.Drawing.Point(66, 72);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(75, 23);
+            this.btnDangXuat.TabIndex = 6;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = false;
+            this.btnDangXuat.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chào mừng đến với Ứng dụng theo dõi sức khoẻ";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 351);
+            this.ClientSize = new System.Drawing.Size(754, 380);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form4";
             this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -140,5 +185,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Label label1;
     }
 }
--- UDTDSK/UDTDSK/Form4.cs
+++ UDTDSK/UDTDSK/Form4.cs
@@ -12,9 +12,89 @@ namespace UDTDSK
 {
     public partial class Form4 : Form
     {
+        Color originalBackColor;
+        Color originalForeColor;
         public Form4()
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void HideLogoutIfNeeded()
+        {
+            if (!pictureBox1.ClientRectangle.Contains(pictureBox1.PointToClient(Cursor.Position)) &&
+                !btnDangXuat.ClientRectangle.Contains(btnDangXuat.PointToClient(Cursor.Position)))
+            {
+                btnDangXuat.Visible = false;
+            }
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            btnDangXuat.Visible = true;
+            btnDangXuat.BringToFront();
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            HideLogoutIfNeeded();
+        }
+        private void btnDangXuat_MouseEnter(object sender, EventArgs e)
+        {
+            btnDangXuat.Visible = true;
+        }
+        private void btnDangXuat_MouseLeave(object sender, EventArgs e)
+        {
+            HideLogoutIfNeeded();
+        }
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            originalBackColor = btn.BackColor;
+            originalForeColor = btn.ForeColor;
+            btn.BackColor = Color.Blue;
+            btn.ForeColor = Color.White;
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = originalBackColor;
+            btn.ForeColor = originalForeColor;
+        }
+        private void AddHoverEffect(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+            {
+                if (ctrl is Button)
+                {
+                    Button btn = (Button)ctrl;
+
+                    btn.MouseEnter += Button_MouseEnter;
+                    btn.MouseLeave += Button_MouseLeave;
+
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.UseVisualStyleBackColor = false;
+                }
+
+                if (ctrl.HasChildren)
+                {
+                    AddHoverEffect(ctrl);
+                }
+            }
+        }
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Left = (splitContainer1.Panel1.Width - pictureBox1.Width) / 2;
+
+            //Xử lý nút Button
+            AddHoverEffect(this);
+
+            // QUAN TRỌNG: đảm bảo event chạy
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += pictureBox1_MouseLeave;
+        }
+        
     }
 }

54d09c50a6272cfcc3f8d9cb941c3c5a945cb72c
tuong vu <vu0230101118@tgu.edu.vn>
2026-05-11 22:20:17 +0700

    update code

--- UDTDSK/UDTDSK/Form9.Designer.cs
+++ UDTDSK/UDTDSK/Form9.Designer.cs
@@ -45,17 +45,6 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-<<<<<<< HEAD
-            this.bt = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtTim = new System.Windows.Forms.TextBox();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-=======
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -68,10 +57,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-<<<<<<< HEAD
-=======
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -97,28 +83,18 @@
             this.splitContainer1.Panel2.Controls.Add(this.button6);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-<<<<<<< HEAD
-            this.splitContainer1.Size = new System.Drawing.Size(1005, 468);
-            this.splitContainer1.SplitterDistance = 334;
-            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.Size = new System.Drawing.Size(920, 380);
             this.splitContainer1.SplitterDistance = 306;
-            this.splitContainer1.SplitterWidth = 3;
-=======
-            this.splitContainer1.Size = new System.Drawing.Size(1227, 468);
-            this.splitContainer1.SplitterDistance = 407;
-            this.splitContainer1.SplitterWidth = 7;
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
+            this.splitContainer1.SplitterWidth = 2;
             this.splitContainer1.TabIndex = 0;
             // 
             // btnDangXuat
             // 
             this.btnDangXuat.BackColor = System.Drawing.Color.SteelBlue;
             this.btnDangXuat.ForeColor = System.Drawing.Color.White;
-            this.btnDangXuat.Location = new System.Drawing.Point(115, 102);
-            this.btnDangXuat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDangXuat.Location = new System.Drawing.Point(86, 83);
             this.btnDangXuat.Name = "btnDangXuat";
-            this.btnDangXuat.Size = new System.Drawing.Size(100, 28);
+            this.btnDangXuat.Size = new System.Drawing.Size(75, 23);
             this.btnDangXuat.TabIndex = 48;
             this.btnDangXuat.Text = "Đăng xuất";
             this.btnDangXuat.UseVisualStyleBackColor = false;
@@ -128,23 +104,20 @@
             // 
             this.button5.BackColor = System.Drawing.Color.RoyalBlue;
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(56, 406);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(42, 330);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(223, 41);
+            this.button5.Size = new System.Drawing.Size(167, 33);
             this.button5.TabIndex = 47;
             this.button5.Text = "Quản lý cảnh báo";
             this.button5.UseVisualStyleBackColor = false;
-            
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.SteelBlue;
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(59, 345);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(44, 280);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(220, 41);
+            this.button4.Size = new System.Drawing.Size(165, 33);
             this.button4.TabIndex = 46;
             this.button4.Text = "Quản lý mục tiêu và theo dõi";
             this.button4.UseVisualStyleBackColor = false;
@@ -154,59 +127,51 @@
             // 
             this.button3.BackColor = System.Drawing.Color.SteelBlue;
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(56, 278);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(42, 226);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(223, 41);
+            this.button3.Size = new System.Drawing.Size(167, 33);
             this.button3.TabIndex = 45;
             this.button3.Text = "Quản lý phân tích tính toán";
             this.button3.UseVisualStyleBackColor = false;
-            
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.SteelBlue;
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(59, 214);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(44, 174);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 41);
+            this.button2.Size = new System.Drawing.Size(165, 33);
             this.button2.TabIndex = 44;
             this.button2.Text = "Quản lý dữ liệu sức khoẻ";
             this.button2.UseVisualStyleBackColor = false;
-           
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.SteelBlue;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(56, 151);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(42, 123);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 41);
+            this.button1.Size = new System.Drawing.Size(167, 33);
             this.button1.TabIndex = 43;
             this.button1.Text = "Thông tin cá nhân";
             this.button1.UseVisualStyleBackColor = false;
-            
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Red;
             this.pictureBox1.Image = global::UDTDSK.Properties.Resources.Avarta;
-            this.pictureBox1.Location = new System.Drawing.Point(101, 22);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(76, 18);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 91);
+            this.pictureBox1.Size = new System.Drawing.Size(92, 74);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 42;
             this.pictureBox1.TabStop = false;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(639, 391);
-            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Location = new System.Drawing.Point(428, 261);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(131, 57);
+            this.button9.Size = new System.Drawing.Size(74, 40);
             this.button9.TabIndex = 9;
             this.button9.Text = "Làm mới";
             this.button9.UseVisualStyleBackColor = true;
@@ -214,10 +179,9 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(505, 75);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Location = new System.Drawing.Point(284, 50);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(100, 28);
+            this.button8.Size = new System.Drawing.Size(56, 19);
             this.button8.TabIndex = 8;
             this.button8.Text = "Tìm";
             this.button8.UseVisualStyleBackColor = true;
@@ -225,169 +189,81 @@
             // 
             // txtTim
             // 
-            this.txtTim.Location = new System.Drawing.Point(119, 78);
-            this.txtTim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTim.Location = new System.Drawing.Point(81, 51);
             this.txtTim.Name = "txtTim";
-            this.txtTim.Size = new System.Drawing.Size(268, 22);
+            this.txtTim.Size = new System.Drawing.Size(152, 20);
             this.txtTim.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 81);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(26, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Tìm kiếm";
             // 
             // dgvCanhBao
             // 
             this.dgvCanhBao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-<<<<<<< HEAD
-            this.dgvCanhBao.Location = new System.Drawing.Point(3, 118);
+            this.dgvCanhBao.Location = new System.Drawing.Point(2, 82);
+            this.dgvCanhBao.Margin = new System.Windows.Forms.Padding(2);
             this.dgvCanhBao.Name = "dgvCanhBao";
             this.dgvCanhBao.Size = new System.Drawing.Size(484, 150);
-            this.dgvCanhBao.TabIndex = 5;
-            this.dgvCanhBao.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCanhBao_CellContentClick);
-            this.dgvCanhBao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCanhBao.Location = new System.Drawing.Point(2, 101);
-            this.dgvCanhBao.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-=======
-            this.dgvCanhBao.Location = new System.Drawing.Point(4, 145);
-            this.dgvCanhBao.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
-            this.dgvCanhBao.Name = "dgvCanhBao";
-            this.dgvCanhBao.Size = new System.Drawing.Size(645, 185);
             this.dgvCanhBao.TabIndex = 5;
             this.dgvCanhBao.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCanhBao_CellContentClick);
             // 
             // bt
             // 
-            this.bt.Location = new System.Drawing.Point(443, 390);
-            this.bt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bt.Location = new System.Drawing.Point(309, 261);
+            this.bt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bt.Name = "bt";
-            this.bt.Size = new System.Drawing.Size(144, 57);
+            this.bt.Size = new System.Drawing.Size(74, 40);
             this.bt.TabIndex = 4;
-            this.bt.Text = "Kiểm tra";
+            this.bt.Text = "Chi tiết";
             this.bt.UseVisualStyleBackColor = true;
             this.bt.Click += new System.EventHandler(this.bt_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(305, 347);
-            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button7.Location = new System.Drawing.Point(176, 261);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-<<<<<<< HEAD
-            this.button7.Size = new System.Drawing.Size(108, 46);
+            this.button7.Size = new System.Drawing.Size(81, 37);
             this.button7.TabIndex = 3;
             this.button7.Text = "Xoá ";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
-            // bt
+            // button6
             // 
-            this.bt.Location = new System.Drawing.Point(395, 279);
-            this.bt.Name = "bt";
-            this.bt.Size = new System.Drawing.Size(75, 41);
-            this.bt.TabIndex = 4;
-            this.bt.Text = "Chi tiết";
-            this.bt.UseVisualStyleBackColor = true;
-            this.bt.Click += new System.EventHandler(this.bt_Click);
-            this.button6.Location = new System.Drawing.Point(48, 317);
+            this.button6.Location = new System.Drawing.Point(53, 261);
             this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(109, 46);
+            this.button6.Size = new System.Drawing.Size(75, 38);
             this.button6.TabIndex = 2;
             this.button6.Text = "Gửi thông báo";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(214, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Quản lý cảnh báo";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 66);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Tìm kiếm";
-            // 
-            // txtTim
-            // 
-            this.txtTim.Location = new System.Drawing.Point(89, 63);
-            this.txtTim.Name = "txtTim";
-            this.txtTim.Size = new System.Drawing.Size(202, 20);
-            this.txtTim.TabIndex = 7;
-            // 
-=======
-            this.button7.Size = new System.Drawing.Size(100, 47);
-            this.button7.TabIndex = 3;
-            this.button7.Text = "Xoá cảnh báo";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
-            // button6
-            // button8
-            // 
-            this.button6.Location = new System.Drawing.Point(71, 347);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 47);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "Gửi thông báo";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            this.button8.Location = new System.Drawing.Point(379, 61);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 23);
-            this.button8.TabIndex = 8;
-            this.button8.Text = "Tìm";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // label1
-            // button9
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(279, 50);
+            this.label1.Location = new System.Drawing.Point(204, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.Size = new System.Drawing.Size(148, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Quản lý cảnh báo";
-<<<<<<< HEAD
-            this.button9.Location = new System.Drawing.Point(479, 318);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(98, 46);
-            this.button9.TabIndex = 9;
-            this.button9.Text = "Làm mới";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-=======
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
             // 
             // Form9
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1227, 468);
+            this.ClientSize = new System.Drawing.Size(920, 380);
             this.Controls.Add(this.splitContainer1);
-<<<<<<< HEAD
-=======
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
->>>>>>> 3cae53b3ee5d4aedd4f3fd6c20e0a5da870dc71a
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form9";
             this.Text = "Form9";
             this.Load += new System.EventHandler(this.Form9_Load);

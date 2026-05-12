3cde234f48a8b078ce5ffbc3f0f04b16625398ba
Tăng Lai Hoàng Vương <vuong022101009@tgu.edu.vn>
2026-05-12 17:49:41 +0700

    8

--- UDTDSK/UDTDSK/Form8.Designer.cs
+++ UDTDSK/UDTDSK/Form8.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Tên mục tiêu");
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Loại mục tiêu");
-            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Mục tiêu");
-            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Thời hạn");
-            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Trạng thái");
+            System.Windows.Forms.ListViewItem listViewItem31 = new System.Windows.Forms.ListViewItem("Tên mục tiêu");
+            System.Windows.Forms.ListViewItem listViewItem32 = new System.Windows.Forms.ListViewItem("Loại mục tiêu");
+            System.Windows.Forms.ListViewItem listViewItem33 = new System.Windows.Forms.ListViewItem("Mục tiêu");
+            System.Windows.Forms.ListViewItem listViewItem34 = new System.Windows.Forms.ListViewItem("Thời hạn");
+            System.Windows.Forms.ListViewItem listViewItem35 = new System.Windows.Forms.ListViewItem("Trạng thái");
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.btnDangXuat = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -148,6 +148,7 @@
             this.button5.TabIndex = 61;
             this.button5.Text = "Quản lý cảnh báo";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -160,6 +161,7 @@
             this.button4.TabIndex = 60;
             this.button4.Text = "Quản lý mục tiêu và theo dõi";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -172,6 +174,7 @@
             this.button3.TabIndex = 59;
             this.button3.Text = "Quản lý phân tích tính toán";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -184,6 +187,7 @@
             this.button2.TabIndex = 58;
             this.button2.Text = "Quản lý dữ liệu sức khoẻ";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -196,6 +200,7 @@
             this.button1.TabIndex = 57;
             this.button1.Text = "Thông tin cá nhân";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -232,11 +237,11 @@
             this.columnHeader4,
             this.columnHeader5});
             this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3,
-            listViewItem4,
-            listViewItem5});
+            listViewItem31,
+            listViewItem32,
+            listViewItem33,
+            listViewItem34,
+            listViewItem35});
             this.listView1.Location = new System.Drawing.Point(30, 55);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(567, 97);
@@ -566,6 +571,7 @@
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form8";
             this.Text = "Form8";
+            this.Load += new System.EventHandler(this.Form8_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
--- UDTDSK/UDTDSK/Form8.cs
+++ UDTDSK/UDTDSK/Form8.cs
@@ -12,9 +12,160 @@ namespace UDTDSK
 {
     public partial class Form8 : Form
     {
+        Color originalBackColor;
+        Color originalForeColor;
         public Form8()
         {
             InitializeComponent();
+        }
+        private void HideLogoutIfNeeded()
+        {
+            if (!pictureBox1.ClientRectangle.Contains(pictureBox1.PointToClient(Cursor.Position)) &&
+                !btnDangXuat.ClientRectangle.Contains(btnDangXuat.PointToClient(Cursor.Position)))
+            {
+                btnDangXuat.Visible = false;
+            }
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            btnDangXuat.Visible = true;
+            btnDangXuat.BringToFront();
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            HideLogoutIfNeeded();
+        }
+        private void btnDangXuat_MouseEnter(object sender, EventArgs e)
+        {
+            btnDangXuat.Visible = true;
+        }
+        private void btnDangXuat_MouseLeave(object sender, EventArgs e)
+        {
+            HideLogoutIfNeeded();
+        }
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có muốn đăng xuất không?",
+                "Xác nhận",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Form1 fr1 = new Form1();
+                fr1.Show();
+                this.Hide();
+            }
+        }
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            originalBackColor = btn.BackColor;
+            originalForeColor = btn.ForeColor;
+            btn.BackColor = Color.Blue;
+            btn.ForeColor = Color.White;
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = originalBackColor;
+            btn.ForeColor = originalForeColor;
+        }
+        private void CenterButtons(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+            {
+                if (ctrl is Button)
+                {
+                    Button btn = (Button)ctrl;
+                    btn.Left = (parent.Width - btn.Width) / 2;
+                }
+
+                if (ctrl.HasChildren)
+                {
+                    CenterButtons(ctrl);
+                }
+            }
+        }
+        private void AddHoverEffect(Control parent)
+        {
+            foreach (Control ctrl in parent.Controls)
+            {
+                if (ctrl is Button)
+                {
+                    Button btn = (Button)ctrl;
+
+                    btn.MouseEnter += Button_MouseEnter;
+                    btn.MouseLeave += Button_MouseLeave;
+
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.UseVisualStyleBackColor = false;
+                }
+
+                if (ctrl.HasChildren)
+                {
+                    AddHoverEffect(ctrl);
+                }
+            }
+        }
+        private void Form8_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Left = (splitContainer1.Panel1.Width - pictureBox1.Width) / 2;
+            btnDangXuat.Left = (splitContainer1.Panel1.Width - btnDangXuat.Width) / 2;
+
+            //Xử lý nút Button
+            AddHoverEffect(this);
+            CenterButtons(splitContainer1.Panel1);
+
+            //Màu viền nút Button Thông tin cá nhân
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.BorderSize = 2;
+            button4.FlatAppearance.BorderColor = Color.Violet;
+
+            // Xử lý button đăng xuất
+            btnDangXuat.Click += btnDangXuat_Click;
+            btnDangXuat.MouseEnter += btnDangXuat_MouseEnter;
+            btnDangXuat.MouseLeave += btnDangXuat_MouseLeave;
+
+            //Xử lý Ảnh
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += pictureBox1_MouseLeave;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form6 fr6 = new Form6();
+            fr6.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form7 fr7 = new Form7();
+            fr7.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form5 fr5 = new Form5();
+            fr5.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form9 fr9 = new Form9();
+            fr9.Show();
+            this.Hide();
         }
     }
 }

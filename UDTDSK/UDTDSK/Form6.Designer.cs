2b6346c0d79d5541809e91747cb3221b2652ff96
Tăng Lai Hoàng Vương <vuong022101009@tgu.edu.vn>
2026-05-12 18:01:57 +0700

    6

--- UDTDSK/UDTDSK/Form6.Designer.cs
+++ UDTDSK/UDTDSK/Form6.Designer.cs
@@ -36,12 +36,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txtCanNang = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radNu = new System.Windows.Forms.RadioButton();
+            this.radNam = new System.Windows.Forms.RadioButton();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtChieuCao = new System.Windows.Forms.TextBox();
+            this.txtTuoi = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -76,12 +79,15 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.radioButton2);
-            this.splitContainer1.Panel2.Controls.Add(this.radioButton1);
-            this.splitContainer1.Panel2.Controls.Add(this.textBox4);
-            this.splitContainer1.Panel2.Controls.Add(this.textBox3);
-            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
-            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button7);
+            this.splitContainer1.Panel2.Controls.Add(this.txtCanNang);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.radNu);
+            this.splitContainer1.Panel2.Controls.Add(this.radNam);
+            this.splitContainer1.Panel2.Controls.Add(this.txtEmail);
+            this.splitContainer1.Panel2.Controls.Add(this.txtChieuCao);
+            this.splitContainer1.Panel2.Controls.Add(this.txtTuoi);
+            this.splitContainer1.Panel2.Controls.Add(this.txtHoTen);
             this.splitContainer1.Panel2.Controls.Add(this.button6);
             this.splitContainer1.Panel2.Controls.Add(this.label6);
             this.splitContainer1.Panel2.Controls.Add(this.label5);
@@ -184,124 +190,150 @@
             this.pictureBox1.TabIndex = 35;
             this.pictureBox1.TabStop = false;
             // 
-            // radioButton2
+            // button7
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(337, 168);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(47, 21);
-            this.radioButton2.TabIndex = 12;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Nữ";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.button7.Location = new System.Drawing.Point(204, 329);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 42);
+            this.button7.TabIndex = 31;
+            this.button7.Text = "Lưu";
+            this.button7.UseVisualStyleBackColor = true;
             // 
-            // radioButton1
+            // txtCanNang
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(241, 168);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 21);
-            this.radioButton1.TabIndex = 11;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Nam";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.txtCanNang.Location = new System.Drawing.Point(255, 253);
+            this.txtCanNang.Name = "txtCanNang";
+            this.txtCanNang.Size = new System.Drawing.Size(219, 22);
+            this.txtCanNang.TabIndex = 30;
             // 
-            // textBox4
+            // label7
             // 
-            this.textBox4.Location = new System.Drawing.Point(241, 245);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(219, 22);
-            this.textBox4.TabIndex = 10;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(152, 256);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Cân nặng:";
             // 
-            // textBox3
+            // radNu
             // 
-            this.textBox3.Location = new System.Drawing.Point(241, 206);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(219, 22);
-            this.textBox3.TabIndex = 9;
+            this.radNu.AutoSize = true;
+            this.radNu.Location = new System.Drawing.Point(351, 180);
+            this.radNu.Name = "radNu";
+            this.radNu.Size = new System.Drawing.Size(47, 21);
+            this.radNu.TabIndex = 28;
+            this.radNu.TabStop = true;
+            this.radNu.Text = "Nữ";
+            this.radNu.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // radNam
             // 
-            this.textBox2.Location = new System.Drawing.Point(241, 123);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(219, 22);
-            this.textBox2.TabIndex = 8;
+            this.radNam.AutoSize = true;
+            this.radNam.Location = new System.Drawing.Point(255, 180);
+            this.radNam.Name = "radNam";
+            this.radNam.Size = new System.Drawing.Size(58, 21);
+            this.radNam.TabIndex = 27;
+            this.radNam.TabStop = true;
+            this.radNam.Text = "Nam";
+            this.radNam.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtEmail
             // 
-            this.textBox1.Location = new System.Drawing.Point(241, 79);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(219, 22);
-            this.textBox1.TabIndex = 7;
+            this.txtEmail.Location = new System.Drawing.Point(255, 290);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(219, 22);
+            this.txtEmail.TabIndex = 26;
+            // 
+            // txtChieuCao
+            // 
+            this.txtChieuCao.Location = new System.Drawing.Point(255, 218);
+            this.txtChieuCao.Name = "txtChieuCao";
+            this.txtChieuCao.Size = new System.Drawing.Size(219, 22);
+            this.txtChieuCao.TabIndex = 25;
+            // 
+            // txtTuoi
+            // 
+            this.txtTuoi.Location = new System.Drawing.Point(255, 135);
+            this.txtTuoi.Name = "txtTuoi";
+            this.txtTuoi.Size = new System.Drawing.Size(219, 22);
+            this.txtTuoi.TabIndex = 24;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(255, 91);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(219, 22);
+            this.txtHoTen.TabIndex = 23;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(241, 311);
+            this.button6.Location = new System.Drawing.Point(351, 329);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(161, 42);
-            this.button6.TabIndex = 6;
+            this.button6.TabIndex = 22;
             this.button6.Text = "Cập nhật thông tin";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(138, 248);
+            this.label6.Location = new System.Drawing.Point(152, 293);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(50, 17);
-            this.label6.TabIndex = 5;
+            this.label6.TabIndex = 21;
             this.label6.Text = "Email: ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(138, 209);
+            this.label5.Location = new System.Drawing.Point(152, 221);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(99, 17);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Số điện thoại: ";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Chiều cao:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(138, 170);
+            this.label4.Location = new System.Drawing.Point(152, 182);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(68, 17);
-            this.label4.TabIndex = 3;
+            this.label4.TabIndex = 19;
             this.label4.Text = "Giới tính: ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(138, 126);
+            this.label3.Location = new System.Drawing.Point(152, 138);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 17);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 18;
             this.label3.Text = "Tuổi:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(138, 82);
+            this.label2.Location = new System.Drawing.Point(152, 94);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 17);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 17;
             this.label2.Text = "Họ tên:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(205, 22);
+            this.label1.Location = new System.Drawing.Point(219, 34);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(276, 29);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 16;
             this.label1.Text = "THÔNG TIN CÁ NHÂN";
             // 
             // Form6
@@ -327,9 +359,6 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnDangXuat;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
@@ -337,15 +366,21 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txtCanNang;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radNu;
+        private System.Windows.Forms.RadioButton radNam;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtChieuCao;
+        private System.Windows.Forms.TextBox txtTuoi;
+        private System.Windows.Forms.TextBox txtHoTen;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
